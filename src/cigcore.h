3054478aed5757e71a7e9ed9dac949cd4af2f7ae
Eigen Lenk <eigen.lenk@gmail.com>
2025-05-04 21:01:11 +0300

    Add option to center stack elements

--- src/cigcore.h
+++ src/cigcore.h
@@ -64,10 +64,12 @@ typedef struct {
   struct {
     enum {
       CIG_LAYOUT_ALIGNS_LEFT = 0,
+      CIG_LAYOUT_ALIGNS_CENTER, /* Only works with v-stack */
       CIG_LAYOUT_ALIGNS_RIGHT
     } horizontal;
     enum {
       CIG_LAYOUT_ALIGNS_TOP = 0,
+      CIG_LAYOUT_ALIGNS_MIDDLE, /* Only works with h-stack */
       CIG_LAYOUT_ALIGNS_BOTTOM
     } vertical;
   } alignment;
