2cf50d738c96e7a27a3c85172d8e026258bdaf5e
Eigen Lenk <eigen.lenk@gmail.com>
2025-05-09 12:42:08 +0300

    Use macros to build welcome window layout
    (cherry picked from commit 7e808673d9dd3fb7b272328a09477692bc463a3c)

--- demo/win95/apps/welcome/welcome.c
+++ demo/win95/apps/welcome/welcome.c
@@ -15,11 +15,14 @@ static window_message_t process_main_window(window_t *this) {
   window_data_t *window_data = (window_data_t*)this->data;
   window_message_t msg = begin_window(this);
 
-  /*  Main vertical stack */
-  if (cig_push_vstack(RECT_AUTO, cig_i_make(14, 16, 10, 16), (cig_layout_params_t) { .spacing = 12 })) {
-
-    /*  Row 1: Large title label */
-    if (cig_push_frame(RECT_AUTO_H(20))) {
+  /*  Using grid to lay out this window. By default, grid items flow to right
+      so we can just append elements and they will jump to the next row when
+      the column in filled. */
+  CIG_GRID(RECT_AUTO, CIG_INSETS(cig_i_make(14, 16, 10, 16)), CIG_PARAMS({
+    CIG_SPACING(12)
+  })) {
+    /* Title text */
+    CIG(RECT_AUTO_H(20)) {
       cig_label(
         (cig_text_properties_t) {
           .font = get_font(FONT_TIMES_NEW_ROMAN_32_BOLD),
@@ -33,70 +36,64 @@ static window_message_t process_main_window(window_t *this) {
         get_font(FONT_FRANKLIN_GOTHIC_BOOK_32),
         get_color(COLOR_WHITE)
       );
-      cig_pop_frame();
     }
 
-    /*  Row 2: Horizontal stack consisting of tip view and stack of buttons next to it */
-    if (cig_push_hstack(RECT_AUTO_H(154), cig_i_uniform(1), (cig_layout_params_t) { .spacing = 12 })) {
-      if (cig_push_frame_insets(RECT_AUTO_W(330), cig_i_make(14, 20, 14, 20))) {
-        cig_fill_panel(get_panel(PANEL_LIGHT_YELLOW), 0);
-        cig_fill_panel(get_panel(PANEL_INNER_BEVEL_NO_FILL), 0);
-        if (cig_push_hstack(RECT_AUTO, cig_i_zero(), (cig_layout_params_t) { .spacing = 14 })) {
-          if (cig_push_frame(cig_r_make(0, 0, 32, 32))) {
-            cig_image(get_image(IMAGE_TIP_OF_THE_DAY), CIG_IMAGE_MODE_CENTER);
-            cig_pop_frame();
-          }
-          if (cig_push_vstack(RECT_AUTO, cig_i_zero(), (cig_layout_params_t) { .spacing = 8 })) {
-            if (cig_push_frame(RECT_AUTO_H(32))) {
-              cig_label((cig_text_properties_t) {
-                .font = get_font(FONT_BOLD),
-                .alignment.horizontal = CIG_TEXT_ALIGN_LEFT
-              }, "Did you know...");
-              cig_pop_frame();
-            }
-            if (cig_push_frame(RECT_AUTO)) {
-              cig_label((cig_text_properties_t) {
-                .alignment.horizontal = CIG_TEXT_ALIGN_LEFT,
-                .alignment.vertical = CIG_TEXT_ALIGN_TOP
-              }, tips[window_data->tip_index]);
-              cig_pop_frame();
-            }
-            cig_pop_frame();
-          }
-          cig_pop_frame();
-        }
-        cig_pop_frame();
-      }
+    /* Tip text and container */
+    CIG(RECT_SIZED(330, 154), CIG_INSETS(cig_i_make(14, 20, 14, 20))) {
+      cig_fill_panel(get_panel(PANEL_LIGHT_YELLOW), 0);
+      cig_fill_panel(get_panel(PANEL_INNER_BEVEL_NO_FILL), 0);
 
-      if (cig_push_vstack(_, cig_i_zero(), (cig_layout_params_t) { .spacing = 6, .height = 23 })) {
-        standard_button(_, "What's New");
-        standard_button(_, "Online Registration");
-        if (standard_button(_, "Next Tip")) {
-          window_data->tip_index = (window_data->tip_index+1)%3;
+      CIG_HSTACK(_, NO_INSETS, CIG_PARAMS({
+        CIG_SPACING(14)
+      })) {
+        CIG(RECT_SIZED(32, 32)) {
+          cig_image(get_image(IMAGE_TIP_OF_THE_DAY), CIG_IMAGE_MODE_CENTER);
         }
-        cig_spacer(58);
-        if (cig_push_frame(RECT_AUTO_H(2))) { /* Separator */
-          cig_fill_panel(get_panel(PANEL_INNER_BEVEL_NO_FILL), 0);
-          cig_pop_frame();
+        CIG_VSTACK(_, NO_INSETS, CIG_PARAMS({
+          CIG_SPACING(8)
+        })) {
+          CIG(RECT_AUTO_H(32)) {
+            cig_label((cig_text_properties_t) {
+              .font = get_font(FONT_BOLD),
+              .alignment.horizontal = CIG_TEXT_ALIGN_LEFT
+            }, "Did you know...");
+          }
+          CIG(_) {
+            cig_label((cig_text_properties_t) {
+              .alignment.horizontal = CIG_TEXT_ALIGN_LEFT,
+              .alignment.vertical = CIG_TEXT_ALIGN_TOP
+            }, tips[window_data->tip_index]);
+          }
         }
-        cig_pop_frame();
       }
-      cig_pop_frame();
     }
 
-    /*  Row 3: Checkbox and close button */
-    if (cig_push_hstack(RECT_AUTO, cig_i_zero(), (cig_layout_params_t) {
-      .spacing = 12,
-      .alignment.vertical = CIG_LAYOUT_ALIGNS_MIDDLE
+    /* List of buttons on the right */
+    CIG_VSTACK(_, CIG_PARAMS({
+      CIG_SPACING(6),
+      CIG_HEIGHT(23)
     })) {
-      checkbox(RECT_AUTO_W(330), NULL, "Show this Welcome Screen next time you start Windows");
-      if (standard_button(RECT_AUTO_H(23), "Close")) {
+      standard_button(_, "What's New");
+      standard_button(_, "Online Registration");
+      if (standard_button(_, "Next Tip")) {
+        window_data->tip_index = (window_data->tip_index+1)%3;
+      }
+      CIG(RECT_FILL) {
+        CIG(RECT(0, CIG_H - 2, CIG_AUTO(), 2)) { /* Separator */
+          cig_fill_panel(get_panel(PANEL_INNER_BEVEL_NO_FILL), 0);
+        }
+      }
+    }
+
+    CIG(RECT_AUTO_W(330)) {
+      checkbox(_, NULL, "Show this Welcome Screen next time you start Windows");
+    }
+
+    CIG(_) {
+      if (standard_button(RECT_CENTERED_VERTICALLY(RECT_AUTO_H(23)), "Close")) {
         msg = WINDOW_CLOSE;
       }
-      cig_pop_frame();
     }
-
-    cig_pop_frame();
   }
 
   end_window();
