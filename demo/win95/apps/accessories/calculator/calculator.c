53d4b737bf57101da333c075d7d0f71cd5f7dfd7
Eigen Lenk <eigen.lenk@gmail.com>
2025-10-15 20:41:48 +0300

    [Demo] Clean up Calculator code

--- demo/win95/apps/accessories/calculator/calculator.c
+++ demo/win95/apps/accessories/calculator/calculator.c
@@ -25,12 +25,18 @@ typedef enum {
 } clear_operation;
 
 typedef enum {
-  MATH_NOOP = 0,
+  MATH_NOOP,
   MATH_ADD,
   MATH_SUBTRACT,
   MATH_MULTIPLY,
   MATH_DIVIDE,
 } math_operation;
+
+typedef enum {
+  MATH_SQRT,
+  MATH_PERCENT,
+  MATH_REPROCICATE
+} math_function;
 
 typedef enum {
   DISPLAY_ENTRY,
@@ -55,6 +61,8 @@ static void perform_clear_operation(app_state*, clear_operation);
 static void enter_number(app_state*, int);
 static void remove_last_number(app_state*);
 static void number_button(app_state*, int);
+static void operation_button(app_state*, math_operation);
+static void function_button(app_state*, math_function);
 static void app_state_update_display(app_state*);
 static void app_state_perform_current_operation(app_state*);
 static void app_state_reprocicate(app_state*);
@@ -66,18 +74,7 @@ application_t
 calculator_app()
 {
   app_state *state = malloc(sizeof(app_state));
-  state->display_mode = DISPLAY_ENTRY;
-  state->entry = 0;
-  state->last_entry = 0;
-  state->accumulator = 0;
-  state->memory = 0;
-  state->has_entry = false;
-  state->user_entry = false;
-  state->percentage = false;
-  state->has_memory = false;
-  state->in_decimal_entry = 0;
-  state->decimal_places = 0;
-  state->operation = MATH_NOOP;
+  memset(state, 0, sizeof(app_state));
   app_state_update_display(state);
 
   return (application_t) {
@@ -97,6 +94,8 @@ calculator_app()
     .flags = KILL_WHEN_PRIMARY_WINDOW_CLOSED
   };
 }
+
+/* PRIVATE METHODS */
 
 static void
 calculator_menubar(window_t *wnd)
@@ -119,6 +118,107 @@ calculator_menubar(window_t *wnd)
 }
 
 static void
+memory_buttons(app_state *state)
+{
+  CIG_VSTACK(_W(36), CIG_PARAMS({
+    CIG_SPACING(6),
+    CIG_ROWS(5)
+  })) {
+    CIG(_H(28)) {
+      cig_fill_style(get_style(STYLE_FILES_CONTENT_BEVEL), 0);
+
+      if (state->has_memory) {
+        cig_draw_label((cig_text_properties) { }, "M");
+      }
+    }
+
+    button_font = get_font(FONT_BOLD);
+    button_title_color = get_color(COLOR_RED);
+
+    if (standard_button(_, "MC")) perform_memory_operation(state, MEMORY_CLEAR);
+    if (standard_button(_, "MR")) perform_memory_operation(state, MEMORY_RECALL);
+    if (standard_button(_, "MS")) perform_memory_operation(state, MEMORY_STORE);
+    if (standard_button(_, "M+")) perform_memory_operation(state, MEMORY_ADD);
+
+    button_font = NULL;
+    button_title_color = NULL;
+  }
+}
+
+static void
+clear_buttons(app_state *state)
+{
+  button_font = get_font(FONT_BOLD);
+  button_title_color = get_color(COLOR_MAROON);
+
+  if (standard_button(_, "C")) perform_clear_operation(state, CLEAR_ALL);
+  if (standard_button(_, "CE")) perform_clear_operation(state, CLEAR_ENTRY | CLEAR_LAST_ENTRY_CONDITIONAL);
+  if (standard_button(_, "Back")) remove_last_number(state);
+
+  button_font = NULL;
+  button_title_color = NULL;
+}
+
+static void
+numeric_function_keypad(app_state *state)
+{
+  button_font = get_font(FONT_BOLD);
+
+  /* First row */
+  number_button(state, 7);
+  number_button(state, 8);
+  number_button(state, 9);
+  operation_button(state, MATH_DIVIDE);
+  function_button(state, MATH_SQRT);
+
+  /* Second row */
+  number_button(state, 4);
+  number_button(state, 5);
+  number_button(state, 6);
+  operation_button(state, MATH_MULTIPLY);
+  function_button(state, MATH_PERCENT);
+
+  /* Third row */
+  number_button(state, 1);
+  number_button(state, 2);
+  number_button(state, 3);
+  operation_button(state, MATH_SUBTRACT);
+  function_button(state, MATH_REPROCICATE);
+
+  /* Fourth row */
+  number_button(state, 0);
+
+  if (standard_button(_, "+/-")) {
+    app_state_flip_sign(state);
+  }
+  
+  if (standard_button(_, ".")) {
+    if (!state->in_decimal_entry) {
+      state->user_entry = true;
+      state->in_decimal_entry = true;
+      state->decimal_places = 0;
+
+      /* Previous operation is cleared when starting to enter a new number */
+      if (state->operation == MATH_NOOP) {
+        state->repeat_operation = MATH_NOOP;
+      }
+    }
+  }
+
+  operation_button(state, MATH_ADD);
+
+  button_title_color = get_color(COLOR_RED);
+
+  if (standard_button(_, "=")) {
+    app_state_perform_current_operation(state);
+  }
+
+  button_title_color = NULL;
+  button_font = NULL;
+}
+
+/* Main window layout including all buttons and menubar */
+static void
 calculator_window_proc(window_t *this, bool window_focused)
 {
   app_state *state = (app_state *)this->owner->data;
@@ -140,8 +240,6 @@ calculator_window_proc(window_t *this, bool window_focused)
     PIN(RIGHT),
     PIN(BOTTOM)
   ), CIG_INSETS(cig_i_uniform(11))) {
-    // cig_fill_color(get_color(COLOR_DEBUG));
-
     /* Input field */
     CIG_RETAIN(input_field, CIG(_H(26), CIG_INSETS(cig_i_horizontal(9))) {
       cig_fill_color(get_color(COLOR_WHITE));
@@ -194,151 +292,6 @@ calculator_window_proc(window_t *this, bool window_focused)
   }
 }
 
-/* PRIVATE METHODS */
-
-static void
-memory_buttons(app_state *state)
-{
-  CIG_VSTACK(_W(36), CIG_PARAMS({
-    CIG_SPACING(6),
-    CIG_ROWS(5)
-  })) {
-    CIG(_H(28)) {
-      cig_fill_style(get_style(STYLE_FILES_CONTENT_BEVEL), 0);
-
-      if (state->has_memory) {
-        cig_draw_label((cig_text_properties) { }, "M");
-      }
-    }
-
-    button_font = get_font(FONT_BOLD);
-    button_title_color = get_color(COLOR_RED);
-
-    if (standard_button(_, "MC")) perform_memory_operation(state, MEMORY_CLEAR);
-    if (standard_button(_, "MR")) perform_memory_operation(state, MEMORY_RECALL);
-    if (standard_button(_, "MS")) perform_memory_operation(state, MEMORY_STORE);
-    if (standard_button(_, "M+")) perform_memory_operation(state, MEMORY_ADD);
-
-    button_font = NULL;
-    button_title_color = NULL;
-  }
-}
-
-static void
-clear_buttons(app_state *state)
-{
-  button_font = get_font(FONT_BOLD);
-  button_title_color = get_color(COLOR_MAROON);
-
-  if (standard_button(_, "C")) perform_clear_operation(state, CLEAR_ALL);
-  if (standard_button(_, "CE")) perform_clear_operation(state, CLEAR_ENTRY | CLEAR_LAST_ENTRY_CONDITIONAL);
-  if (standard_button(_, "Back")) remove_last_number(state);
-
-  button_font = NULL;
-  button_title_color = NULL;
-}
-
-static void
-numeric_function_keypad(app_state *state)
-{
-  button_font = get_font(FONT_BOLD);
-
-  button_title_color = get_color(COLOR_BLUE);
-  number_button(state, 7);
-  number_button(state, 8);
-  number_button(state, 9);
-
-  button_title_color = get_color(COLOR_RED);
-  if (standard_button(_, "/")) {
-    if (state->has_entry) {
-      state->accumulator = state->entry;
-    }
-    perform_clear_operation(state, CLEAR_ENTRY);
-    state->percentage = false;
-    state->operation = MATH_DIVIDE;
-    state->display_mode = DISPLAY_ACCUMULATOR;
-    app_state_update_display(state);
-  }
-
-  button_title_color = get_color(COLOR_NAVY);
-  if (standard_button(_, "sqrt")) { app_state_sqrt(state); }
-
-  button_title_color = get_color(COLOR_BLUE);
-  number_button(state, 4);
-  number_button(state, 5);
-  number_button(state, 6);
-
-  button_title_color = get_color(COLOR_RED);
-  if (standard_button(_, "*")) {
-    if (state->has_entry) {
-      state->accumulator = state->entry;
-    }
-    perform_clear_operation(state, CLEAR_ENTRY);
-    state->percentage = false;
-    state->operation = MATH_MULTIPLY;
-    state->display_mode = DISPLAY_ACCUMULATOR;
-    app_state_update_display(state);
-  }
-
-  button_title_color = get_color(COLOR_NAVY);
-  if (standard_button(_, "%")) { app_state_set_or_calculate_percentage(state); }
-
-  button_title_color = get_color(COLOR_BLUE);
-  number_button(state, 1);
-  number_button(state, 2);
-  number_button(state, 3);
-
-  button_title_color = get_color(COLOR_RED);
-  if (standard_button(_, "-")) {
-    if (state->has_entry) {
-      state->accumulator = state->entry;
-    }
-    perform_clear_operation(state, CLEAR_ENTRY);
-    state->percentage = false;
-    state->operation = MATH_SUBTRACT;
-    state->display_mode = DISPLAY_ACCUMULATOR;
-    app_state_update_display(state);
-  }
-
-
-  button_title_color = get_color(COLOR_NAVY);
-  if (standard_button(_, "1/x")) { app_state_reprocicate(state); }
-
-  button_title_color = get_color(COLOR_BLUE);
-  number_button(state, 0);
-  
-  if (standard_button(_, "+/-")) { app_state_flip_sign(state); }
-  
-  if (standard_button(_, ".")) {
-    if (!state->in_decimal_entry) {
-      state->in_decimal_entry = true;
-      state->decimal_places = 0;
-      /* Previous operation is cleared when starting to enter a new number */
-      if (state->operation == MATH_NOOP) {
-        state->repeat_operation = MATH_NOOP;
-      }
-    }
-  }
-
-  button_title_color = get_color(COLOR_RED);
-  
-  if (standard_button(_, "+")) {
-    if (state->has_entry) {
-      state->accumulator = state->entry;
-    }
-    perform_clear_operation(state, CLEAR_ENTRY);
-    state->percentage = false;
-    state->operation = MATH_ADD;
-    state->display_mode = DISPLAY_ACCUMULATOR;
-    app_state_update_display(state);
-  }
-
-  if (standard_button(_, "=")) { app_state_perform_current_operation(state); }
-
-  button_title_color = NULL;
-  button_font = NULL;
-}
-
 /* [[ OPERATIONS ]] */
 
 static void
@@ -379,9 +332,7 @@ static void
 perform_clear_operation(app_state *state, clear_operation op)
 {
   if (op & CLEAR_ENTRY) {
-    printf("CLEAR_ENTRY\n");
     if (state->has_entry && op & CLEAR_LAST_ENTRY_CONDITIONAL) {
-      printf("\talso clears last entry\n");
       state->last_entry = 0;
       state->operation = MATH_NOOP;
     }
@@ -398,20 +349,18 @@ perform_clear_operation(app_state *state, clear_operation op)
   }
 
   if (op & CLEAR_OPERATION) {
-    printf("CLEAR_OPERATION\n");
     state->operation = MATH_NOOP;
     state->repeat_operation = MATH_NOOP;
   }
 
   if (op & CLEAR_LAST_ENTRY) {
-    printf("CLEAR_LAST_ENTRY\n");
     state->last_entry = 0;
   }
 
   app_state_update_display(state);
 }
 
-/* [[ NUMBER ENTRY ]] */
+/* [[ NUMBER EDITING ]] */
 
 static void
 enter_number(app_state *state, int n)
@@ -461,15 +410,62 @@ remove_last_number(app_state *state)
   app_state_update_display(state);
 }
 
+/* [[ BUTTON COMPONENTS ]] */
+
 static void
 number_button(app_state *state, int n)
 {
   const char *numbers[] = {"0","1","2","3","4","5","6","7","8","9"};
 
+  button_title_color = get_color(COLOR_BLUE);
+
   if (standard_button(_, numbers[n])) {
     enter_number(state, n);
   }
 }
+
+static void
+operation_button(app_state *state, math_operation op)
+{
+  const char *operations[] = {"+","-","*","/"};
+
+  button_title_color = get_color(COLOR_RED);
+
+  if (standard_button(_, operations[op-1])) {
+    if (state->has_entry) {
+      app_state_perform_current_operation(state);
+    }
+    perform_clear_operation(state, CLEAR_ENTRY);
+    state->percentage = false;
+    state->operation = op;
+    state->display_mode = DISPLAY_ACCUMULATOR;
+    app_state_update_display(state);
+  }
+}
+
+static void
+function_button(app_state *state, math_function func)
+{
+  const char *funcs[] = {"sqrt","%","1/x"};
+
+  button_title_color = get_color(COLOR_NAVY);
+
+  if (standard_button(_, funcs[func])) {
+    switch (func) {
+    case MATH_SQRT:
+      app_state_sqrt(state);
+      break;
+    case MATH_PERCENT:
+      app_state_set_or_calculate_percentage(state);
+      break;
+    case MATH_REPROCICATE:
+      app_state_reprocicate(state);
+      break;
+    }
+  }
+}
+
+/* [[ APP STATE MUTATION ]]*/
 
 static void
 app_state_update_display(app_state *this)
@@ -514,24 +510,18 @@ app_state_perform_current_operation(app_state *this)
     }
     break;
   case MATH_ADD:
-    printf("%lf + %lf\n", this->accumulator, value);
     this->accumulator += value;
     break;
   case MATH_SUBTRACT:
-    printf("%lf - %lf\n", this->accumulator, value);
     this->accumulator -= value;
     break;
   case MATH_MULTIPLY:
-    printf("%lf * %lf\n", this->accumulator, value);
     this->accumulator *= value;
     break;
   case MATH_DIVIDE:
-    printf("%lf / %lf\n", this->accumulator, value);
     this->accumulator /= value;
     break;
   }
-
-  printf("\tAccumulator: %lf\n", this->accumulator);
 
   perform_clear_operation(this, CLEAR_ENTRY);
   
